76c38b64f54ad3f6a1a20a3bd142874f1e023dfc
Maliha Fatima <malihameer@hotmail.com>
2024-04-27 14:19:37 -0700

    box shadow

--- medisearch/components/searchBox.jsx
+++ medisearch/components/searchBox.jsx
@@ -19,12 +19,12 @@ export default function SearchBox({ onSearch }) {
       <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
       <div class="relative">
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <IoSearchSharp className="w-5 h-5 text-gray-500 dark:text-gray-400" /> {/* React Icon used */}
+          <IoSearchSharp className="w-5 h-5 text-gray-900 dark:text-gray-400" /> {/* React Icon used */}
         </div>
         <input
           type="search"
           id="default-search"
-          class="block w-full px-20 py-3 text-sm text-black-900 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-300 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="block w-full px-20 py-3 text-sm text-black-900 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white-300 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 shadow-lg"
           placeholder="Search.."
           required
           value={query}
