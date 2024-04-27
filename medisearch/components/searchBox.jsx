c2a19747e1795648dff6cae53b747a70b34e2771
Nasser Ben <nasserbenyed@gmail.com>
2024-04-27 15:16:12 -0700

    add fda api into search

--- medisearch/components/searchBox.jsx
+++ medisearch/components/searchBox.jsx
@@ -1,23 +1,37 @@
-"use client";
+import React, { useState } from "react";
 
-import { useState } from "react";
-
-export default function SearchBox({ onSearch }) {
+export default function SearchBox({ setDrugData }) {
   const [query, setQuery] = useState("");
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
-  const handleSearch = () => {
-    onSearch(query);
+  const handleSearch = async () => {
+    try {
+      const apiUrl = `https://api.fda.gov/drug/label.json?search=indications_and_usage:"${query}"&limit=1`;
+
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        if (response.status === 404) {
+          setDrugData(`No Results Found found for ${query}!`);
+        } else {
+          throw new Error("Failed to fetch drug information");
+        }
+      } else {
+        const drugInfo = await response.json();
+        setDrugData(drugInfo);
+      }
+    } catch (error) {
+      console.error("Error fetching drug information:", error.message);
+    }
   };
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center justify-center mt-8">
       <input
         type="text"
-        placeholder="Search..."
+        placeholder="Search for medication or symptom..."
         value={query}
         onChange={handleInputChange}
         className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
