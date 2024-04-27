608ea5cc93fee74bf51b2557d3fcc8f5c7ac7f92
Maliha Fatima <malihameer@hotmail.com>
2024-04-27 14:02:32 -0700

    updated search bar

--- medisearch/components/searchBox.jsx
+++ medisearch/components/searchBox.jsx
@@ -1,6 +1,6 @@
-"use client";
-
-import { useState } from "react";
+"use client"
+import React, { useState } from 'react';
+import { IoSearchSharp } from 'react-icons/io5';
 
 export default function SearchBox({ onSearch }) {
   const [query, setQuery] = useState("");
@@ -9,25 +9,33 @@ export default function SearchBox({ onSearch }) {
     setQuery(event.target.value);
   };
 
-  const handleSearch = () => {
+  const handleSearch = (event) => {
+    event.preventDefault();  // Prevent the default form submission behavior
     onSearch(query);
   };
 
   return (
-    <div className="flex items-center">
-      <input
-        type="text"
-        placeholder="Search..."
-        value={query}
-        onChange={handleInputChange}
-        className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-      <button
-        onClick={handleSearch}
-        className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-      >
-        Search
-      </button>
-    </div>
+    <form onSubmit={handleSearch} class="max-w-md mx-auto"> {/* Form for handling the submission */}
+      <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+      <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <IoSearchSharp className="w-5 h-5 text-gray-500 dark:text-gray-400" /> {/* React Icon used */}
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          class="block w-full px-20 py-3 text-sm text-black-900 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-300 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search.."
+          required
+          value={query}
+          onChange={handleInputChange}  // Handling input change
+        />
+        {/* <button
+          type="submit"
+          class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Search
+        </button> */}
+      </div>
+    </form>
   );
 }
