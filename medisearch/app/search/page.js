7dc761fa1b362f0ac7927e3c6d6c225414dca8dc
jeralsonep <jeralsoneparedes@gmail.com>
2024-04-27 19:30:51 -0700

    fetching info, WIP

--- medisearch/app/search/page.js
+++ medisearch/app/search/page.js
@@ -1,84 +1,28 @@
+"use client";
 
+import { useState } from "react";
 import SearchBox from "@/components/searchBox";
 import Header from "@/components/header";
+import Results from "@/components/results";
 
 export default function Search(){
+
+    const [drugData, setDrugData] = useState(null);
+
     return(
         <div className="bg-custom-bg">
-        <div className="flex flex-start ">
-        <div className="mt-10 ml-20 mr-20 flex flex-start">
-            <Header />
-        </div>
-        <div className="mt-10 mr-10 ml-40">
-            <SearchBox />
-        </div>
-    </div> 
-        <p className="ml-20 mb-5">Results for ...</p>
-  
-        <div className="ml-20 mr-20">
-                <div className="flex grid grid-cols-2 md:grid-cols-3 gap-8 ">
-                    <div className="w-30 h-32 bg-blue-200 flex  rounded-lg">
-                        <div className="flex flex-col flex-start">
-                        <span className="ml-5 mt-2 mb-2 text-black font-bold text-xl">Ibuprofen</span>
-                        <span className="ml-5" >Ibuprofen tablets are indicated for relief of the signs and symptoms of rheumatoid arthritis and osteoarthritis...</span>
-                        </div>
-                    </div>
-                    <div className="w-30 h-32 bg-blue-200 flex  rounded-lg">
-                        <div className="flex flex-col flex-start">
-                        <span className="ml-5 mt-2 mb-2 text-black font-bold text-xl">Ibuprofen</span>
-                        <span className="ml-5" >Ibuprofen tablets are indicated for relief of the signs and symptoms of rheumatoid arthritis and osteoarthritis...</span>
-                        </div>
-                    </div>
-                    <div className="w-30 h-32 bg-blue-200 flex  rounded-lg">
-                        <div className="flex flex-col flex-start">
-                        <span className="ml-5 mt-2 mb-2 text-black font-bold text-xl">Ibuprofen</span>
-                        <span className="ml-5" >Ibuprofen tablets are indicated for relief of the signs and symptoms of rheumatoid arthritis and osteoarthritis...</span>
-                        </div>
-                    </div>
-                    <div className="w-30 h-32 bg-blue-200 flex  rounded-lg">
-                        <div className="flex flex-col flex-start">
-                        <span className="ml-5 mt-2 mb-2 text-black font-bold text-xl">Ibuprofen</span>
-                        <span className="ml-5" >Ibuprofen tablets are indicated for relief of the signs and symptoms of rheumatoid arthritis and osteoarthritis...</span>
-                        </div>
-                    </div>
-                    <div className="w-30 h-32 bg-blue-200 flex  rounded-lg">
-                        <div className="flex flex-col flex-start">
-                        <span className="ml-5 mt-2 mb-2 text-black font-bold text-xl">Ibuprofen</span>
-                        <span className="ml-5" >Ibuprofen tablets are indicated for relief of the signs and symptoms of rheumatoid arthritis and osteoarthritis...</span>
-                        </div>
-                    </div>
-                    <div className="w-30 h-32 bg-blue-200 flex  rounded-lg">
-                        <div className="flex flex-col flex-start">
-                        <span className="ml-5 mt-2 mb-2 text-black font-bold text-xl">Ibuprofen</span>
-                        <span className="ml-5" >Ibuprofen tablets are indicated for relief of the signs and symptoms of rheumatoid arthritis and osteoarthritis...</span>
-                        </div>
-                    </div>
-                    <div className="w-30 h-32 bg-blue-200 flex  rounded-lg">
-                        <div className="flex flex-col flex-start">
-                        <span className="ml-5 mt-2 mb-2 text-black font-bold text-xl">Ibuprofen</span>
-                        <span className="ml-5" >Ibuprofen tablets are indicated for relief of the signs and symptoms of rheumatoid arthritis and osteoarthritis...</span>
-                        </div>
-                    </div>
-                    <div className="w-30 h-32 bg-blue-200 flex  rounded-lg">
-                        <div className="flex flex-col flex-start">
-                        <span className="ml-5 mt-2 mb-2 text-black font-bold text-xl">Ibuprofen</span>
-                        <span className="ml-5" >Ibuprofen tablets are indicated for relief of the signs and symptoms of rheumatoid arthritis and osteoarthritis...</span>
-                        </div>
-                    </div>
-                    <div className="w-30 h-32 bg-blue-200 flex  rounded-lg">
-                        <div className="flex flex-col flex-start">
-                        <span className="ml-5 mt-2 mb-2 text-black font-bold text-xl">Ibuprofen</span>
-                        <span className="ml-5" >Ibuprofen tablets are indicated for relief of the signs and symptoms of rheumatoid arthritis and osteoarthritis...</span>
-                        </div>
-                    </div>
-
-                    {/* Add more grid items as needed */}
+            <div className="flex flex-start ">
+                <div className="mt-10 ml-20 mr-20 flex flex-start">
+                    <Header />
                 </div>
+                <div className="mt-10 mr-10 ml-40">
+                    <SearchBox setDrugData={setDrugData} />
+                </div>
+            </div> 
+            <p className="ml-20 mb-5">Results for ... WIP</p>
+            <div className="ml-20 mr-20">
+                {<Results data={drugData} />}
             </div>
-    </div>
-
-
-        
-       
+        </div>
     );
 }
--- medisearch/components/results.jsx
+++ medisearch/components/results.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 
 export default function Results({ data }) {
-  // Check if data is null or undefined before accessing its properties
-  if (!data || !data.results || !data.results[0]) {
-    // Render a message or placeholder if data is not available
+
+  function capitalizeFirstLetter(string) {
+    return string.replace(/\b\w/g, (char) => char.toUpperCase());
+  }
+
+  if (!data || !data.results || data.results.length === 0) {
+
     return (
       <div className="mt-8">
         <p>No drug information available.</p>
       </div>
     );
   }
-
-  // Extract generic name from data
-  const genericName = data.results[0].openfda?.generic_name?.[0];
-  // Extract indications and usage from data
-  const indicationsAndUsage = data.results[0].indications_and_usage?.[0];
+  console.log(data);
 
   return (
-    <div className="mt-8">
-      {/* Display generic name if available */}
-      {genericName && <p>Generic Name: {genericName}</p>}
-      {/* Display indications and usage if available */}
-      {indicationsAndUsage && <p>Indications and Usage: {indicationsAndUsage}</p>}
+    <div className="mt-8 grid grid-cols-2 md:grid-cols-3 gap-8">
+      {data.results.map((result, index) => (
+        <div key={index} className="w-full h-32 bg-blue-200 flex rounded-lg">
+          <div className="flex flex-col flex-start">
+            <span className="ml-5 mt-2 mb-2 text-black font-bold text-xl">
+              {capitalizeFirstLetter(result.openfda?.brand_name?.[0]?.substring(0, 50))}
+              {result.openfda?.brand_name?.[0]?.length > 50 ? '...' : ''}
+            </span>
+            <span className="ml-5">{result.indications_and_usage?.[0]?.replace(/•/g, '').substring(0, 170)}{result.indications_and_usage?.[0]?.length > 170 ? '...' : ''}</span>
+
+          </div>
+      </div>
+      ))}
     </div>
   );
 }
